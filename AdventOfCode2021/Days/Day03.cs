3ab35c09f09c9dcf6915f5d76d5314ce690ed07b
Davide Testoni <davide.testoni@mlcube.com>
2022-12-14 19:31:12 +0100

    Day 3 part 2 (2021)

--- AdventOfCode2021/Days/Day03.cs
+++ AdventOfCode2021/Days/Day03.cs
@@ -4,6 +4,8 @@
     {
         public void Run()
         {
+            // TODO: Check if a binary tree can help
+
             var lines = File.ReadAllLines("Days/Day03.txt");
             var lineLength = lines[0].Length;
             var bitSum = Enumerable.Repeat(0, lineLength).ToArray();
@@ -22,6 +24,49 @@
             var epsilonValue = BinaryStringToInt(string.Join(string.Empty, epsilonBits));
 
             Console.WriteLine(gammaValue * epsilonValue);
+
+            var mostCommonBits = lines.Select(l => l.ToCharArray()).ToList();
+            var leastCommonBits = lines.Select(l => l.ToCharArray()).ToList();
+
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                if (mostCommonBits.Count == 1)
+                {
+                    break;
+                }
+
+                var oneCount = mostCommonBits
+                    .Where(s => s[i] == '1').Count();
+
+                var zeroCount = mostCommonBits.Count - oneCount;
+                var mostCommon = oneCount >= zeroCount ? '1' : '0';
+                
+                mostCommonBits.RemoveAll(s => s[i] != mostCommon);
+            }
+
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                if (leastCommonBits.Count == 1)
+                {
+                    break;
+                }
+
+                var oneCount = leastCommonBits
+                    .Where(s => s[i] == '1').Count();
+
+                var zeroCount = leastCommonBits.Count - oneCount;
+                var leastCommon = oneCount >= zeroCount ? '0' : '1';
+
+                leastCommonBits.RemoveAll(s => s[i] != leastCommon);
+            }
+
+            var oxygenGenRating = BinaryStringToInt(
+                new string(mostCommonBits.Single()));
+            
+            var co2ScrubberRating = BinaryStringToInt(
+                new string(leastCommonBits.Single()));
+
+            Console.WriteLine(oxygenGenRating * co2ScrubberRating);
         }
 
         private static int BinaryStringToInt(string binaryString)
